0be8a9533466db7423fe77a56b4c7ee9ee2ab91e
Pantelis Isaiah <karatheodory@gmail.com>
2018-07-29 13:33:02 -0400

    First steps

---
+++ examples/dot_access.py
@@ -0,0 +1,33 @@
+# Library module(s)
+from pprint import pprint
+# Other module(s)
+from ezdict import EZDict
+
+# A dictionary
+d = {'first': 'John',
+     'last': 'Doe'}
+
+# Create an "easy" dictionary
+ezd = EZDict.from_dict(d)
+# Check the contents of the easy dictionary.
+print(f'ezd = {ezd}')
+# Easily access values
+print(f'ezd.first = {ezd.first}')
+# Easily set values
+ezd.first = 'Mary'
+print(f'ezd.first = {ezd.first}')
+
+# A dictionary with a nested dictionary
+dd = {'first': 'John',
+      'last': 'Doe',
+      'phones': {
+          'work': 416,
+          'home': 523
+      }}
+
+ezdd = EZDict.from_dict(dd)
+
+# Check the contents of the easy dictionary.
+print(f'ezdd = {ezdd}')
+# Access a nested value
+print(f'ezdd.phones.work = {ezdd.phones.work}')
---
+++ ezdict.py
@@ -0,0 +1,51 @@
+"""
+date: 2018-07-29
+author: karatheodory@gmail.com
+
+This is a simple class derived from the python built-in `dict`; it's sole purpose is to function as a dictionary but
+also provide access to the dictionary values using the dot notation:
+
+If (key, val) is an item in an ezdict d, then `val` can be accessed either by `d[key]` or by `d.key`.
+
+There are obvious limitations:
+
+    #.  For `d.key` to work `key` can only be such that `d.key` is a legal expression.
+    #.  For `d.key` to work *correctly*, `d.key` should refer to the same object as `d[key]`. Think, e.g., of `key`
+        being a string containing spaces.
+    #.  The function that internally dispatches `d.key` will *intercept method calls*. If you want specific methods to
+        continue being available,
+"""
+
+
+class EZDict(dict):
+
+    def __init__(self):
+        super(EZDict, self).__init__()
+
+    # def __init__(self, d):
+    #
+    #     super(EZDict, self).__init__()
+    #     self = EZDict.from_dict(d)
+
+    def __getattr__(self, item):
+        return super(EZDict, self).get(item)
+
+    def __setattr__(self, key, value):
+        return super(EZDict, self).__setitem__(key, value)
+
+    @classmethod
+    def from_dict(cls, d: dict):
+
+        ezd = EZDict()
+
+        for key, value in d.items():
+
+            if isinstance(value, dict):
+
+                ezd[key] = EZDict.from_dict(value)
+
+            else:
+
+                ezd[key] = value
+
+        return ezd
