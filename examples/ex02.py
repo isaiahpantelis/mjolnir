1d6c9f3a553dfa1a82b135d1cc1b49eadae2d278
Pantelis Isaiah <karatheodory@gmail.com>
2018-09-16 09:30:55 -0400

    Added casting to EZConfig

--- examples/ex02.py
+++ examples/ex02.py
@@ -5,5 +5,21 @@ from ezconfig import EZConfig
 
 fpath = './config.ini'
 
-ezcfg = EZConfig(fpath)
-pprint(ezcfg.read())
+# # Use #1: get back all sections
+# ezcfg = EZConfig(fpath).read()
+# print(f'ezcfg.data.cds_folder = {ezcfg.data.cds_folder}')
+#
+# # Use #2: get back a specific section
+# ezcfg = EZConfig(fpath).read().data
+# print(f'ezcfg = {ezcfg}')
+
+# Test casting
+ezcfg = EZConfig(fpath).read()
+
+for section, conts in ezcfg.items():
+    print(f'section: {section} ')
+    for key, val in conts.items():
+        print(f'    {key}: {val}')
+        # print(f'    {type(key)}: {type(val)}')
+
+print(ezcfg.datain.xfolder)
--- ezconfig.py
+++ ezconfig.py
@@ -14,13 +14,51 @@ class EZConfig:
         self.cfgparser = configparser.ConfigParser(interpolation=interpolation_obj)
         self.fpath = fpath
 
-    def read(self):
+    def _cast(self, x):
+        """
+        Auxiliary function used to cast strings read from the config file to more useful types. For example, it will
+        convert any of the strings true, TRUE, True, etc, to True (bool).
 
+        :param x:
+        :return:
+        """
+
+        # At the moment, it is guaranteed that x is a string, however we may want to add a check in the future in case
+        # unexpected uses.
+
+        # Logical
+        cases = ['true', 'yes', 'on']
+        if x.lower() in cases:
+            return True
+
+        cases = ['false', 'no', 'off']
+        if x.lower() in cases:
+            return False
+
+        try:
+            y = float(x)
+            return y
+        except ValueError:
+            return x
+
+        return x
+
+    def read(self):
+        """
+        It returns a new object instead of adding data to self. The reason is that if the data read from the config file
+        were added to self as a dictionary, then access with the '.' operator would involve one more arbitrarily named
+        variable: self.X = ...
+
+        :return:
+        """
+
+        # Read the config file using the objects config parser that was created during initialisation.
         self.cfgparser.read(self.fpath)
 
+        # Start building the return value of the function.
         tmp_dict = {}
 
         for section in self.cfgparser.sections():
-            tmp_dict[section] = {key: val for key, val in self.cfgparser[section].items()}
+            tmp_dict[section] = {key: self._cast(val) for key, val in self.cfgparser[section].items()}
 
         return EZDict.from_dict(tmp_dict)
