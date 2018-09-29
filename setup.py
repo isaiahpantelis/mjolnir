9c2b7a1fed8e0b2f453f9b2cc4188bb1abcdd5e5
Pantelis Isaiah <karatheodory@gmail.com>
2018-09-29 06:04:13 -0400

    Added setup file for easy distribution

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name="mjolnir",
+      version="0.0.1",
+      py_modules=['ezconfig', 'ezdict'],
+      scripts=['examples/ezconfig_ex.py', 'examples/ezdict_ex.py'], )
